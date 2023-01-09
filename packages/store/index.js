34bfe3356915d96eea67cfabb39798996fd91628
liukun09 <liukun09@qianxin.com>
2023-01-10 00:42:00 +0800

    feat: 发布订阅, 离线缓存完成

--- packages/store/index.js
+++ packages/store/index.js
@@ -1,13 +1,15 @@
 const createStore = ()=> {
     const store = {}
-    const offlineList = []
+    let offlineList = []
     const offlineDispatch = ()=> {
-        const func = offlineList.shift()
-        func()
+        offlineList.forEach(func => {
+            console.log('func :>> ', func);
+            func()
+        });
     }
     const offlineSubscribe = (func)=> {
         offlineList.push(func)
-        console.log('offlineList :>> ', offlineList);
+        // console.log('offlineList :>> ', offlineList);
         // return unSubscribe.bind(null, eventName)
     }
 
@@ -34,132 +36,141 @@ const createStore = ()=> {
         });
     }
     return {
-        subscribe: (eventName, func)=> {
-            subscribe(eventName, func, store)
+        subscribe: (eventName, val)=> {
+            subscribe(eventName, val, store)
+            if(offlineList.length > 0) {
+                offlineList.forEach(func=> {
+                    func()
+                })
+            }
+            offlineList = []
         },
-        dispatch: (eventName, val)=> {
+        dispatch(eventName, val) {
             dispatch(eventName, val, store)
+            const fn = dispatch.bind(this, eventName, val, store)
+            offlineList.push(fn)
         },
     }
 }
 
-// const store = createStore()
+const store = createStore()
 
-// store.dispatch('click', 'dispatch something1')
-// const unSubscribe = store.subscribe('click', (val)=> {
-//     console.log('subscribe :>> ', val);
-// })
-// // unSubscribe()
-// store.dispatch('click', 'dispatch something2')
-
-
-const Observer = (function Observer() {
-    const _default = 'default'
-    const Event = (function Event() {
-        const namespaceCache = {}
-        const each = (ary, fn) => {
-            let ret
-            for (let i = 0, len = ary.length; i < len; i += 1) {
-                const n = ary[i]
-                ret = fn.call(n, i, n)
-            }
-            return ret
-        }
-        const _listen = (key, fn, cache) => {
-            if (!cache[key]) {
-                cache[key] = []
-            }
-            cache[key].push(fn)
-        }
-        const _remove = (key, cache, fn) => {
-            if (cache[key]) {
-                if (fn) {
-                    for (let i = cache[key].length; i >= 0; i -= 1) {
-                        if (cache[key][i] === fn) {
-                            cache[key].splice(i, 1)
-                        }
-                    }
-                } else {
-                    cache[key] = []
-                }
-            }
-        }
-        const _trigger = (...rest) => {
-            const [cache, key, ...args] = rest
-            const stack = cache[key]
-            if (!stack || !stack.length) {
-                return
-            }
-            return each(stack, (index, fn) => fn(...args))
-        }
-        const create = (namespace) => {
-            namespace = namespace || _default
-            const cache = {}
-            let offlineStack = [] // 离线事件
-            const ret = {
-                listen(key, fn, last) {
-                    _listen(key, fn, cache)
-                    if (offlineStack === null) {
-                        return
-                    }
-                    if (last === 'last') {
-                        offlineStack.length && offlineStack.pop()()
-                    } else {
-                        each(offlineStack, (index, func) => {
-                            func()
-                        })
-                    }
-                    offlineStack = null
-                },
-                one(key, fn, last) {
-                    _remove(key, cache)
-                    this.listen(key, fn, last)
-                },
-                remove(key, fn) {
-                    _remove(key, cache, fn)
-                },
-                trigger(...rest) {
-                    rest.unshift(cache)
-                    const fn = () => _trigger.apply(this, rest)
-                    if (offlineStack) {
-                        return offlineStack.push(fn)
-                    }
-                    return fn()
-                },
-            }
-            const namespaceRet = namespaceCache[namespace]
-                ? namespaceCache[namespace]
-                : (namespaceCache[namespace] = ret)
-            return namespace ? namespaceRet : ret
-        }
-        return {
-            create,
-            one(key, fn, last) {
-                const event = this.create()
-                event.one(key, fn, last)
-            },
-            remove(key, fn) {
-                const event = this.create()
-                event.remove(key, fn)
-            },
-            listen(key, fn, last) {
-                const event = this.create()
-                event.listen(key, fn, last)
-            },
-            trigger(...rest) {
-                const event = this.create()
-                event.trigger.apply(this, rest)
-            },
-        }
-    })()
-    return Event
-})()
-/** ************ 先发布后订阅 ******************* */
-Observer.trigger('click', 1)
-Observer.listen('click', (a) => {
-    console.log(a) // 输出：1
+store.dispatch('click', 'dispatch something1')
+const unSubscribe = store.subscribe('click', (val)=> {
+    console.log('subscribe :>> ', val);
 })
-/** ************ 使用命名空间 ******************* */
+// unSubscribe()
+store.dispatch('click', 'dispatch something2')
+
+// const Observer = (function Observer() {
+//     const _default = 'default'
+//     const Event = (function Event() {
+//         const namespaceCache = {}
+//         const each = (ary, fn) => {
+//             let ret
+//             for (let i = 0, len = ary.length; i < len; i += 1) {
+//                 const n = ary[i]
+//                 ret = fn.call(n, i, n)
+//             }
+//             return ret
+//         }
+//         const _listen = (key, fn, cache) => {
+//             if (!cache[key]) {
+//                 cache[key] = []
+//             }
+//             cache[key].push(fn)
+//         }
+//         const _remove = (key, cache, fn) => {
+//             if (cache[key]) {
+//                 if (fn) {
+//                     for (let i = cache[key].length; i >= 0; i -= 1) {
+//                         if (cache[key][i] === fn) {
+//                             cache[key].splice(i, 1)
+//                         }
+//                     }
+//                 } else {
+//                     cache[key] = []
+//                 }
+//             }
+//         }
+//         const _trigger = (...rest) => {
+//             const [cache, key, ...args] = rest
+//             const stack = cache[key]
+//             if (!stack || !stack.length) {
+//                 return
+//             }
+//             return each(stack, (index, fn) => fn(...args))
+//         }
+//         const create = (namespace) => {
+//             namespace = namespace || _default
+//             const cache = {}
+//             let offlineStack = [] // 离线事件
+//             const ret = {
+//                 listen(key, fn, last) {
+//                     _listen(key, fn, cache)
+//                     if (offlineStack === null) {
+//                         return
+//                     }
+//                     if (last === 'last') {
+//                         offlineStack.length && offlineStack.pop()()
+//                     } else {
+//                         each(offlineStack, (index, func) => {
+//                             func()
+//                         })
+//                     }
+//                     offlineStack = null
+//                 },
+//                 one(key, fn, last) {
+//                     _remove(key, cache)
+//                     this.listen(key, fn, last)
+//                 },
+//                 remove(key, fn) {
+//                     _remove(key, cache, fn)
+//                 },
+//                 trigger(...rest) {
+//                     rest.unshift(cache)
+//                     const fn = _trigger.bind(this, ...rest)
+//                     if (offlineStack) {
+//                         return offlineStack.push(fn)
+//                     }
+//                     return fn()
+//                 },
+//             }
+//             const namespaceRet = namespaceCache[namespace]
+//                 ? namespaceCache[namespace]
+//                 : (namespaceCache[namespace] = ret)
+//             return namespace ? namespaceRet : ret
+//         }
+//         return {
+//             create,
+//             one(key, fn, last) {
+//                 const event = this.create()
+//                 event.one(key, fn, last)
+//             },
+//             remove(key, fn) {
+//                 const event = this.create()
+//                 event.remove(key, fn)
+//             },
+//             listen(key, fn, last) {
+//                 const event = this.create()
+//                 event.listen(key, fn, last)
+//             },
+//             trigger(...rest) {
+//                 const event = this.create()
+//                 event.trigger.apply(this, rest)
+//             },
+//         }
+//     })()
+//     return Event
+// })()
+// /** ************ 先发布后订阅 ******************* */
+// Observer.trigger('click', 1)
+// Observer.trigger('click', 2)
+// Observer.trigger('click', 3)
+// Observer.listen('click', (a) => {
+//     console.log(a) // 输出：1
+// })
+// /** ************ 使用命名空间 ******************* */
 // Observer.create('namespace1').listen(
 //     'click',
 //     (a) => {
